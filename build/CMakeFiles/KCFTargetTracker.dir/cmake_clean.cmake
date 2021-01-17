89e4aa22746b0dbad8a31dd98e36dc0a87d6a20f
potBagMeat <369230018@qq.com>
2021-01-17 05:40:53 +0000

    add source files

---
+++ build/CMakeFiles/KCFTargetTracker.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KCFTargetTracker.dir/target_tracking.cpp.o"
+  "CMakeFiles/KCFTargetTracker.dir/tracking_utility.cpp.o"
+  "KCFTargetTracker.pdb"
+  "KCFTargetTracker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KCFTargetTracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/kcf_tracker.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kcf_tracker.dir/KCFcpp/src/fhog.cpp.o"
+  "CMakeFiles/kcf_tracker.dir/KCFcpp/src/kcftracker.cpp.o"
+  "libkcf_tracker.pdb"
+  "libkcf_tracker.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kcf_tracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
